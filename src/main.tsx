fd2297247b84ed992dc959dacffc3fcf625b6aca
Anders Bille Lind <anders.bille.lind@gmail.com>
2022-10-03 21:01:15 +0100

    feat: React router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
+/* React dependencies */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+/* Routing Dependencies */
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+/* Global Styling */
 import './index.css'
 
+/* Routing pages */
+import App from './App'
+
+const routes = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+	},
+])
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={routes} />
+	</React.StrictMode>
 )
