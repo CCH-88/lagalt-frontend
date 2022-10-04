be4231c35ca00579c6cd05c5f5fed110808d4bb7
Tobiasnm <toeb1992@live.dk>
2022-10-04 15:42:45 +0200

    more navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './NavBar/Navbar'
+import MainView from './views/MainView'
+import ProfileView from './views/ProfileView'
 
 function App() {
 	return (
 		<BrowserRouter>
-		<Navbar/>
+			<div className="App">
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<MainView />} />
+					<Route path="/profile" element={<ProfileView />} />
+				</Routes>
+			</div>
 		</BrowserRouter>
 	)
 }
--- src/NavBar/Navbar.tsx
+++ src/NavBar/Navbar.tsx
@@ -3,13 +3,15 @@ import { NavLink } from "react-router-dom"
 const Navbar = () => {
     return (
         <nav className="border-gray-200 px-2 sm:px-4 py-3 bg-gray-100 w-100 h-16">
-            <NavLink to='/' className="float-left text-blue-500 text-2xl my-auto font-bold">Lagalt</NavLink>
-            <div className="inline-block float-right w-1/3">
-                <button className="w-100 px-4 py-1 rounded-full text-center border-gray-800 border float-right mx-5">Sign up</button>
-                <button className="w-100 px-4 py-1 rounded-full text-center border-gray-800 border float-right mx-5">Login</button>
-                <NavLink to='/profile' className="w-100 px-4 py-1 rounded-full text-center border-gray-800 border float-right mx-5">Profile</NavLink>
+            <div className="inline-block w-1/3">
+                <NavLink to='/' className="inline-block text-blue-500 text-2xl my-auto font-bold">Lagalt</NavLink>
             </div>
-            <input type="text" className="inline-block py-0.5 bg-slate-300 rounded-full text-left px-8 w-1/3 float-right text-2xl" placeholder="🔎Search" />
+            <input type="text" className="inline-block py-0.5 bg-slate-300 rounded-full text-left px-8 w-1/3 text-2xl" placeholder="🔎Search" />
+            <div className="flex-auto float-right w-1/3">
+                <button className="px-2 py-1 rounded-full text-center border-gray-800 border mx-3 float-right">Sign up</button>
+                <button className="px-2 py-1 rounded-full text-center border-gray-800 border mx-3 float-right">Login</button>
+                <NavLink to='/profile' className="px-2 py-1 rounded-full text-center border-gray-800 border mx-3 float-right">Profile</NavLink>
+            </div>
         </nav >
     )
 }
