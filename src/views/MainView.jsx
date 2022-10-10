19e533d72f0c86b5a792e838134a163cfe51e2b6
Christian Casper Hofma <cchofma@live.dk>
2022-10-10 14:53:20 +0200

    Added project cards to top of page

--- src/views/MainView.jsx
+++ src/views/MainView.jsx
@@ -1,9 +1,11 @@
+import MainViewComponent from "../components/MainView/MainViewComponent"
 
 const MainView = () => {
     
     return (
         <>
-            <p>MainView</p>
+            <h1 className="font-bold text-3xl mb-1 p-4">Current projects</h1> 
+            <MainViewComponent />          
         </>
     )
 }
