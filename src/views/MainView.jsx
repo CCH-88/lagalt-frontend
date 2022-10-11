3faff512c0a33a6e46e4c3763f5795b3b4e2af79
Christian Casper Hofma <cchofma@live.dk>
2022-10-11 15:17:01 +0200

    Added projectlist

--- src/views/MainView.jsx
+++ src/views/MainView.jsx
@@ -1,11 +1,19 @@
-import MainViewComponent from "../components/MainView/MainViewComponent"
+import ProjectList from "../components/MainView/ProjectList"
+import TopProjects from "../components/MainView/TopProjects"
+
 
 const MainView = () => {
     
     return (
         <>
-            <h1 className="font-bold text-3xl mb-1 p-4">Current projects</h1> 
-            <MainViewComponent />          
+            <h1 className="font-bold text-3xl mb-1 p-4">Projects</h1> 
+            <TopProjects />
+            <br></br>
+            <h2 className="font-bold text-3xl mb-1 p-4">List of Projects</h2>
+            <br></br>
+            <ProjectList />
+            
+                      
         </>
     )
 }
