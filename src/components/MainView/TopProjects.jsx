142e1eca3c6336fad6267b82f4a442163ad02ee3
Christian Casper Hofma <cchofma@live.dk>
2022-10-17 14:03:59 +0200

    Added slice to map shows three trending projects

--- src/components/MainView/TopProjects.jsx
+++ src/components/MainView/TopProjects.jsx
@@ -1,39 +1,17 @@
 import { checkProject, insertProject } from "../../api/project";
 import { storageSave } from "../../utils/storage";
-//import {  } from 'react-router-dom';
 import { useState, useEffect } from "react";
 
-/*async function onLoad() {
-  const [error, projectName] = await insertProject();
-  
-  //Local state
-  //If something went wrong while fetching data....
-  const [ apiError, setApiError ] = useState(null);
-  
-  
-  if(error !== null){    
-    //setApiError = error;
-    setApiError(error)
-    console.log(setApiError);
-  }
-  if(projectName !== null){
-    storageSave('aProject', projectName)
-  }
-
-  console.log('Error', error);
-  console.log('Project name', projectName);
-}*/
-
 function TopProjects() {
-  const [posts, setPosts] = useState([]);
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     fetch("https://633fd672d1fcddf69caaa419.mockapi.io/api/v1/project")
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        setPosts(data);
-      })
+        setProjects(data);
+      })      
       .catch((err) => {
         console.log(err.message);
       });
@@ -41,19 +19,20 @@ function TopProjects() {
 
   return (
     <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-      {posts.map(function (post) {
+      { 
+        projects.slice(0,3).map(function (project) {        
         return (
           <div className="rounded overflow-hidden shadow-lg bg-white">
             <div className="rounded overflow-hidden shadow-lg bg-white">
               <img
                 className="w-full"
-                src={post.images}
-                alt="Forest"
+                src={project.images}
+                alt="Picture could not load"
               ></img>
 
               <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{post.name}</div>
-                <p className="text-gray-700 text-base">{post.description}</p>
+                <div className="font-bold text-xl mb-2">{project.name}</div>
+                <p className="text-gray-700 text-base">{project.description}</p>
               </div>
 
               <div className="px-6 pt-4 pb-2">
