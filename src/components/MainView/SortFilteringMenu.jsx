69137d47bee188a3b88ca85f9d5901564b95f185
Anders Bille Lind <anders.bille.lind@gmail.com>
2022-10-20 10:46:58 +0200

    fix: class to className

--- src/components/MainView/SortFilteringMenu.jsx
+++ src/components/MainView/SortFilteringMenu.jsx
@@ -14,7 +14,7 @@ function SortFilteringMenu() {
           </button>
         </li>
         <li className="mr-3">
-          <select class="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm">
+          <select className="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm">
             <option value="">Categories</option>
             <option value="for-rent">Music</option>
             <option value="for-sale">Film</option>
