9df349f77f2c0bcae183476a29c674b44622b3b4
Anders Bille Lind <anders.bille.lind@gmail.com>
2022-10-20 13:32:09 +0200

    feat: keycloak withAuth

--- src/hoc/withAuth.jsx
+++ src/hoc/withAuth.jsx
@@ -1,14 +1,20 @@
-import { Navigate } from "react-router-dom";
-import { STORAGE_KEY_TOKEN } from "../const/storageKeys";
-import { useUser } from "../context/UserContext";
+import { Navigate } from 'react-router-dom'
+import { STORAGE_KEY_TOKEN } from '../const/storageKeys'
+import { useUser } from '../context/UserContext'
+import { useKeycloak } from '@react-keycloak/web'
 
 const withAuth = (Component) => (props) => {
-  const { user } = useUser();
-  if (localStorage.getItem(STORAGE_KEY_TOKEN)) {
-    //maybe different authentication method
-    return <Component {...props} />;
-  } else {
-    return <Navigate to="/" />;
-  }
-};
-export default withAuth;
+	const { user } = useUser()
+	const { keycloak } = useKeycloak()
+
+  console.log(keycloak)
+
+	if (keycloak.authenticated) {
+    console.log(keycloak)
+		return <Component {...props} />
+	} else {
+		return <Navigate to="/" />
+	}
+
+}
+export default withAuth
