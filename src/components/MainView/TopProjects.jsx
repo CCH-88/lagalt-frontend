dded7ab8f29eb028f841c11f6f0eb0a6e05cee7b
Christian Casper Hofma <cchofma@live.dk>
2022-10-24 13:38:58 +0200

    Missing div fixed...

--- src/components/MainView/TopProjects.jsx
+++ src/components/MainView/TopProjects.jsx
@@ -21,7 +21,7 @@ function TopProjects() {
   return (
     <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
       {projects.slice(0, 3).map(function (project) {
-        return (
+        return (          
           <div className="rounded overflow-hidden shadow-lg bg-white">
             <div className="rounded overflow-hidden shadow-lg bg-white">
               <img
@@ -34,7 +34,7 @@ function TopProjects() {
                 <div className="font-bold text-xl mb-2">{project.name}</div>
                 <p className="text-gray-700 text-base">{project.description}</p>
               </div>
-
+            </div>
 						<div className="px-6 pt-4 pb-2">
 							<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
 							<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
@@ -42,8 +42,9 @@ function TopProjects() {
 						</div>
 					</div>
 				)
+        
 			})}
-		</div>
+    </div>
 	)
 }
 export default TopProjects
