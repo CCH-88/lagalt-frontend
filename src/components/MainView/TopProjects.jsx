77c224ce808338ce11b5ea401c80c7aeb6b3e548
Christian Casper Hofma <cchofma@live.dk>
2022-10-24 13:40:05 +0200

    Formated

--- src/components/MainView/TopProjects.jsx
+++ src/components/MainView/TopProjects.jsx
@@ -21,7 +21,7 @@ function TopProjects() {
   return (
     <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
       {projects.slice(0, 3).map(function (project) {
-        return (          
+        return (
           <div className="rounded overflow-hidden shadow-lg bg-white">
             <div className="rounded overflow-hidden shadow-lg bg-white">
               <img
@@ -35,16 +35,21 @@ function TopProjects() {
                 <p className="text-gray-700 text-base">{project.description}</p>
               </div>
             </div>
-						<div className="px-6 pt-4 pb-2">
-							<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-							<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-							<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#fall</span>
-						</div>
-					</div>
-				)
-        
-			})}
+            <div className="px-6 pt-4 pb-2">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #photography
+              </span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #travel
+              </span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #fall
+              </span>
+            </div>
+          </div>
+        );
+      })}
     </div>
-	)
+  );
 }
-export default TopProjects
+export default TopProjects;
