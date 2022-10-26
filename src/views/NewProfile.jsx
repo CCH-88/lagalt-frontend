9ddce8e72a476d17c0af1e183d2862513af1e25c
Anders Bille Lind <anders.bille.lind@gmail.com>
2022-10-26 19:12:04 +0200

    feat: creation of user from auth user

--- src/views/NewProfile.jsx
+++ src/views/NewProfile.jsx
@@ -1,22 +1,42 @@
 import withAuth from '../hoc/withAuth'
 import { useKeycloak } from '@react-keycloak/web'
 import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 const NewProfile = () => {
-	const { keycloak } = useKeycloak()
+	const { keycloak, initialized } = useKeycloak()
 	const navigate = useNavigate()
+	const [loading, setLoading] = useState(false)
 
-	fetch(import.meta.env.VITE_BACKEND_URL + '/api/v1/freelancers', {
-		method: 'POST',
-		headers: {
-			Authorization: `Bearer ${keycloak.token}`,
-		},
-	}).then(navigate(`/profile/${keycloak.subject}`))
-
+	async function createUser() {
+		setLoading(true)
+		try {
+			fetch(import.meta.env.VITE_BACKEND_URL + '/api/v1/freelancers', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${keycloak.token}`,
+				},
+				body: JSON.stringify({
+					username: keycloak.tokenParsed.preferred_username,
+					email: keycloak.tokenParsed.email,
+				}),
+			}).then(navigate(`/profile/${keycloak.subject}`))
+			setLoading(false)	
+		} catch (error) {
+			console.error(error)
+		}
+		
+	}
 	return (
-		<>
-			<h2 className="text-center mt-10 text-2xl ">Creating User in the Backend</h2>
-		</>
+		<div className='w-full flex flex-col justify-center'>
+
+			<h2 className="text-center mt-10 text-2xl ">To fully use our platfor you need to Create your User in our Backend</h2>
+			<ul id='errors'></ul>
+			<button type="button" onClick={createUser} disabled={!initialized && keycloak.authenticated | loading} className="base-btn mt-10 mx-auto text-white bg-primary-blue">
+				Create Backend User from Auth user.
+			</button>
+		</div>
 	)
 }
 
